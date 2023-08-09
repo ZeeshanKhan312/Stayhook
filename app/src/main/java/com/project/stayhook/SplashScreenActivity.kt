848b5298e49a7cbee664467afc4deaa633021a4a
zeeshan <zeeshankalimkhan@gmail.com>
2023-08-10 00:18:48 +0530

    created layout grdient background and Splash Screen activity

---
+++ app/src/main/java/com/project/stayhook/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.project.stayhook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.project.stayhook.intro.IntroPageActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+//        //remove title bar form up
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity, IntroPageActivity::class.java)
+            startActivity(intent)
+            finish() //if press back so this will not reappear
+        }, 3000)
+
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
+}
---
+++ app/src/main/java/com/project/stayhook/intro/IntroPageActivity.kt
@@ -0,0 +1,12 @@
+package com.project.stayhook.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.stayhook.R
+
+class IntroPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_page)
+    }
+}
